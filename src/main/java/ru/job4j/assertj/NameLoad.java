7ab9590c29fef5f218e6dbba542050ec5d36ad61
Stepan-Zazyan <stepanzazyan@yandex.ru>
2022-08-27 19:03:40 +0300

    Утверждения с исключениями [#504886 #332555]

--- src/main/java/ru/job4j/assertj/NameLoad.java
+++ src/main/java/ru/job4j/assertj/NameLoad.java
@@ -6,6 +6,15 @@ import java.util.stream.Collectors;
 public class NameLoad {
     private final Map<String, String> values = new HashMap<>();
 
+
+    public boolean hello(int num) {
+        return num > 5;
+    }
+
+    public void nameMethod(String name) {
+        System.out.println(name);
+    }
+
     public void parse(String... names) {
         if (names.length == 0) {
             throw new IllegalArgumentException("Names array is empty");
@@ -21,7 +30,7 @@ public class NameLoad {
                 )));
     }
 
-    private boolean validate(String name) {
+    public boolean validate(String name) {
         if (!name.contains("=")) {
             throw new IllegalArgumentException(
                     String.format("this name: %s does not contain the symbol \"=\"", name));
--- src/test/java/ru/job4j/assertj/NameLoadTest.java
+++ src/test/java/ru/job4j/assertj/NameLoadTest.java
@@ -1,7 +1,7 @@
 package ru.job4j.assertj;
 
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class NameLoadTest {
     @Test
@@ -11,4 +11,34 @@ class NameLoadTest {
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessageContaining("no data");
     }
+
+   /*не узнает метод parse, просит сделать статичным в итоге рушатся поля,
+    ипользуемые в методе, да и не в этом решение.. думаю*/
+    @Test
+    void checkParseEmptyName() {
+        String[] ar = {};
+        assertThatThrownBy(NameLoad.parse(ar))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    /*Не видит мой тестовый метод bool,выделяет красным,
+    пришлось вот так изворачивиться. но как это использовать для исключений?*/
+    @Test
+    void boolTest() {
+        int num = 6;
+        boolean res = new NameLoad().hello(num);
+        assertThat(res).isTrue();
+    }
+
+   /*При обычной подстановке  assertThatThrownBy(validate(name))
+    получаем cannot resolve method validate..почему?
+    вот такое тоже не получается, required & provided не сходятся.*/
+    @Test
+    void validateContains() {
+        String name = "w=p";
+        Exception ex = new IllegalStateException();
+        assertThatThrownBy(ex)
+                .isInstanceOf(IllegalStateException.class);
+    }
+
 }
