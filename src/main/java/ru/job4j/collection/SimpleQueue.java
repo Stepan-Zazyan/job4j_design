a9c6963ffa9c107889ef3d7740ab63d8226ee998
Stepan-Zazyan <stepanzazyan@yandex.ru>
2022-12-31 17:38:49 +0300

    5. Очередь на двух стеках [#160 #376279]

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -1,26 +1,29 @@
 package ru.job4j.collection;
 
-import java.util.NoSuchElementException;
-
 public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
 
+    private int countIn = 0;
+    private int countOut = 0;
+
     public T poll() {
-        //проверка на удаление из пустого объкта SimpleStack<>()
-        // почему компилятор сообщает что он никогда не null?
-        if (in == null) {
-            throw new NoSuchElementException();
-        }
+        countIn--;
         return in.pop();
     }
 
     public void push(T value) {
-        //переложить тарелки 1 2 3 в другую стопку 3 2 1
-        out.push(in.pop());
-        //добавить значение 4 в новую стопку в начало 4 3 2 1
+        while (countIn > 0) {
+            out.push(in.pop());
+            countOut++;
+            countIn--;
+        }
         out.push(value);
-        //перекинуть в изначальную стопку из другой стопки 1 2 3 4
-        in.push(out.pop());
+        countOut++;
+        while (countOut > 0) {
+            in.push(out.pop());
+            countOut--;
+            countIn++;
+        }
     }
 }
