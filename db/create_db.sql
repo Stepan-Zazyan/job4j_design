ca174284c8dd7d607243020faa92beb994693051
Stepan-Zazyan <stepanzazyan@yandex.ru>
2023-04-23 14:39:25 +0300

    1. Схема прав пользователи и роли. [#1093]

--- db/create_db.sql
+++ db/create_db.sql
@@ -1,60 +1,71 @@
-<<<<<<< HEAD
-select version();
-create table flights (
-                         id serial primary key,
-                         model integer,
-                         city text,
-                         distance numeric
-);
-insert into flights (model, city, distance) values (134, 'ТУ', 500.0);
-update flights
-set model = 747;
-select * from flights;
-delete from flights;
-=======
-
-create table users (
-    id serial primary key ,
-    role_id int references roles(id)
-);
-
-create table roles (
-    id serial primary key,
-    role_name text,
-    rule_id int references rules(id)
-);
-
-create table rules (
-    id serial primary key,
+/*users - role = many-to-one
+role - rules = many-to-many
+item - users = many-to-one
+item - comments = one-to-many
+item - attachs = one-to-many
+item - category = many-to-one
+item - state = many-to-one*/
+create table rules
+(
+    id   serial primary key,
     rule text
 );
 
-create table comments (
-    id serial primary key,
-    comment text
+create table roles
+(
+    id        serial primary key,
+    role_name text
 );
 
-create table attachs (
+create table rules_n_roles
+(
     id serial primary key,
-    attach text
+    role_id int references roles(id),
+    rule_id int references rules(id)
 );
 
-create table category (
-    id serial primary key,
+create table users
+(
+    id      serial primary key,
+    role_id int references roles (id)
+);
+
+create table category
+(
+    id       serial primary key,
     category int
 );
 
-create table state (
-    id serial primary key,
+create table state
+(
+    id    serial primary key,
     state text
 );
 
-create table item (
-    id serial primary key,
-    user_id int references users(id),
-    comment_id int references comments(id),
-    attach_id int references attachs(id),
-    category_id int references category(id),
-    state_id int references state(id)
+create table item
+(
+    id          serial primary key,
+    user_id     int references users (id),
+    category_id int references category (id),
+    state_id    int references state (id)
 );
->>>>>>> 11ff1e0 (1. Схема прав пользователи и роли. [#1093])
+
+drop table comments;
+drop table attachs;
+
+create table comments
+(
+    id      serial primary key,
+    item_id int references item(id),
+    comment text
+);
+
+create table attachs
+(
+    id     serial primary key,
+    item_id int references item(id),
+    attach text
+);
+
+
+
--- db/insert.sql
+++ db/insert.sql
@@ -1,9 +1,9 @@
 insert into rules (rule) values ('правило пользователя');
-select * from rules;
-insert into roles (role_name, rule_id) values ('обычный пользователь', 1);
+insert into roles (role_name) values ('обычный пользователь');
+insert into rules_n_roles (role_id, rule_id) values (1, 1);
 insert into users (role_id) values (1);
-insert into comments (comment) values ('comment');
-insert into attachs (attach) values ('attach');
 insert into category (category) values (2);
 insert into state (state) values ('state');
-insert into item (user_id, comment_id, attach_id, category_id, state_id) values (1,1,1,1,1);
+insert into item (user_id, category_id, state_id) values (1,1,1);
+insert into comments (item_id, comment) values (1, 'comment');
+insert into attachs (item_id, attach) values (1, 'attach');
