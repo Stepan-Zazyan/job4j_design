1f8ec5c8521fea224039c5c5176c887ebbccbf47
Stepan-Zazyan <stepanzazyan@yandex.ru>
2023-04-23 23:11:32 +0300

    0.1. Связи между таблицами [#373615]

--- db/one_to_one.sql
+++ db/one_to_one.sql
@@ -1,13 +1,13 @@
 create table vin
 (
     id serial primary key,
-    number int
+    number int unique
 );
-
+drop table vin;
+    drop table car_passport;
 create table car_passport
 (
     id     serial primary key,
-    vin_id int references vin(id)
+    vin_id int references vin(id) unique
 );
-
 
