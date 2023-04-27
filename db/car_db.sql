e4c8778a58f9b99bf3bcb403c510d2f9079aa7c5
Stepan-Zazyan <stepanzazyan@yandex.ru>
2023-04-27 14:14:41 +0300

    1. Представления [#504792]

--- db/car_db.sql
+++ db/car_db.sql
@@ -44,23 +44,24 @@ values ('auto'),
 
 
 insert into cars (name, body_id, engine_id, transmission_id)
-VALUES ('toyota camry',         null, 1, 1),
-       ('toyota corolla',       1, null, 2),
-       ('toyota lobster',     1, 2, 2),
-       ('toyota hipster',     null, 2, null),
-       ('kia rio',            1, 3, 2),
-       ('kia cerato',         1, null, 2),
-       ('kia lol',              1, 2, 2),
-       ('kia boob',             1, 2, null);
+VALUES ('toyota camry', null, 1, 1),
+       ('toyota corolla', 1, null, 2),
+       ('toyota lobster', 1, 2, 2),
+       ('toyota hipster', null, 2, null),
+       ('kia rio', 1, 3, 2),
+       ('kia cerato', 1, null, 2),
+       ('kia lol', 1, 2, 2),
+       ('kia boob', 1, 2, null);
 
 select c.id, c.name, cb.name, ce.name, ct.name
-from cars as c LEFT join car_bodies cb on c.body_id = cb.id
-left JOIN car_engines ce on ce.id = c.engine_id
-LEFT join car_transmissions ct on c.transmission_id = ct.id;
+from cars as c
+         LEFT join car_bodies cb on c.body_id = cb.id
+         left JOIN car_engines ce on ce.id = c.engine_id
+         LEFT join car_transmissions ct on c.transmission_id = ct.id;
 
 select b.name
 from cars as c
-right join car_bodies as b on b.id = c.body_id
+         right join car_bodies as b on b.id = c.body_id
 where c.body_id is null;
 
 
@@ -73,3 +74,14 @@ select t.name
 from cars as c
          right join car_transmissions as t on t.id = c.transmission_id
 where c.transmission_id is null;
+
+create view sedan_auto_transmission_sum
+as
+select cb.name as body_name, sum(cb.id)
+from cars as c
+         join car_bodies as cb on c.body_id = cb.id
+join car_transmissions ct on c.transmission_id = ct.id
+where transmission_id = 2
+group by cb.name;
+
+select  * from sedan_auto_transmission_sum;
