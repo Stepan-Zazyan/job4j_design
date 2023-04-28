8b0077ed5c55a20b5eb1b95fadab3a521066d504
Stepan-Zazyan <stepanzazyan@yandex.ru>
2023-04-28 17:24:21 +0300

    2. Триггеры [#504803]

--- db/objects_db/triggers.sql
+++ db/objects_db/triggers.sql
@@ -15,32 +15,47 @@ values ('огурец', 'land', 5, 50),
        ('tomato', 'land', 10, 80),
        ('milk', 'cow', 2, 100);
 
-create trigger tax_trigger
-    after insert
-    on products
-    referencing new table as inserted
-    for each statement
-execute procedure tax();
-
-create or replace function tax()
+create or replace function tax_for_statement()
     returns trigger as
 $$
 begin
     update products
-    set price = price + price * 0.13;
+    set count = count + count * 2;
     return new;
 end;
 $$
     language 'plpgsql';
 
+create trigger tax_trigger
+    after insert
+    on products
+    referencing new table as inserted
+    for each statement
+execute procedure tax_for_statement();
+
 insert into products (name, producer, count, price)
-values ('potatoes', 'land', 8, 100);
+values ('onion', 'land', 20, 1000);
+
+create or replace function
+    tax_before_paste()
+    returns trigger as
+$$
+begin
+    update products
+    set count = count + count * 2;
+    return new;
+end;
+$$
+    language 'plpgsql';
 
 create trigger tax_trigger_before
     before insert
     on products
     for each row
-execute procedure tax();
+execute procedure tax_before_paste();
+
+insert into products (name, producer, count, price)
+values ('onion', 'land', 8, 100);
 
 create table history_of_price
 (
