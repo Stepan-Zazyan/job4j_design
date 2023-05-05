c1bfd314f9df7bc21120a9c4ca324b2c7cbb8e56
Stepan-Zazyan <stepanzazyan@yandex.ru>
2023-05-05 12:40:13 +0300

    0. JDBC [#6863]

--- src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -9,11 +9,11 @@ import java.sql.SQLException;
 
 public class ConnectionDemo {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Class.forName("org.postgresql.Driver");
-        String url = "jdbc:postgresql://localhost:5432/idea_db";
         String path = "./data/app.properties";
         Config config = new Config(path);
         config.load();
+        Class.forName(config.value("driver"));
+        String url = config.value("url");
         String login = config.value("login");
         String password = config.value("password");
         try (Connection connection = DriverManager.getConnection(url, login, password)) {
