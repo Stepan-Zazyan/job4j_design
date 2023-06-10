1355f338c342f5e2643875a256dc4c7037e4e0d9
Stepan-Zazyan <stepanzazyan@yandex.ru>
2023-06-10 11:21:23 +0300

    12. Валидация параметров запуска [#246865 [#246865]]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,8 +10,12 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".java")).
+        Search s = new Search();
+        s.validateLength(args);
+        s.validateValues(args[0], args[1]);
+        Path start = Paths.get(args[0]);
+        String ends = args[1];
+        search(start, p -> p.toFile().getName().endsWith(ends)).
                 forEach(System.out::println);
     }
 
@@ -18,5 +23,27 @@ public class Search {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public void validateLength(String[] strins) {
+        if (strins.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage  ROOT_FOLDER.");
+        }
+        if (strins.length == 1) {
+            throw new IllegalArgumentException("Set file extension in configuration");
+        }
+    }
+
+    public void validateValues(String str0, String str1) {
+        File file = new File(str0);
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        if (!str1.startsWith(".")) {
+            throw new IllegalArgumentException("File extension must start with \".\"");
+        }
     }
 }
