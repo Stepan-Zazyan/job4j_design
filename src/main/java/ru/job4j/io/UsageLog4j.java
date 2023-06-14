e055fc39bf67e9dbc027c97e183ecef3421a60a7
Stepan-Zazyan <stepanzazyan@yandex.ru>
2023-06-14 19:01:16 +0300

    3. Slf4j - вывод переменных. [#268852  [#268852]]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,16 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Stepan Zazyan";
+        int age = 33;
+        byte b = 127;
+        short s = 16000;
+        double d = 1.23;
+        float f = 3.2f;
+        long l = 900000;
+        char c = 'a';
+        LOG.debug("User info name : {}, age : {}, "
+                + "b : {}, s : {}, d : {}, f : {}, "
+                + "l : {}, c : {},", name, age, b, s, d, f, l, c);
     }
 }
