3230eb9d754ce495ce26c98b9f51d3a070d1837e
Stepan-Zazyan <stepanzazyan@yandex.ru>
2023-07-08 14:33:19 +0300

    2. Принципы Kiss, Dry и Yagni [#238813 # [#238813]]

--- src/test/java/ru/job4j/kiss/MaxMinTest.java
+++ src/test/java/ru/job4j/kiss/MaxMinTest.java
@@ -10,7 +10,7 @@ class MaxMinTest {
 
     @Test
     void max() {
-        List<Integer> list = List.of(1,4,3,2);
+        List<Integer> list = List.of(1, 4, 3, 2);
         int max = 4;
         MaxMin<Integer> maxMin = new MaxMin<>();
         int result = maxMin.max(list, Integer::compareTo);
@@ -19,7 +19,7 @@ class MaxMinTest {
 
     @Test
     void min() {
-        List<Integer> list = List.of(1,4,3,2);
+        List<Integer> list = List.of(1, 4, 3, 2);
         int min = 1;
         MaxMin<Integer> maxMin = new MaxMin<>();
         int result = maxMin.min(list, Integer::compareTo);
