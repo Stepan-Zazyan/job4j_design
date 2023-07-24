32d7513ad603507cce5c41f0d3ce0805cd75dc30
Stepan-Zazyan <stepanzazyan@yandex.ru>
2023-07-24 10:15:51 +0300

    2. Принципы Kiss, Dry и Yagni [#238813  [#238813 #428641]]

--- src/main/java/ru/job4j/kiss/MaxMin.java
+++ src/main/java/ru/job4j/kiss/MaxMin.java
@@ -1,31 +1,30 @@
 package ru.job4j.kiss;
+
+import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.BiPredicate;
 
-public class MaxMin<T> implements Comparable<MaxMin<T>> {
-    int value;
 
-    public T searchMinMax(List<T> list, BiPredicate<T, T> function) {
+public class MaxMin {
+
+    public <T> T searchMin(List<T> list, Comparator<T> comparator) {
+        return strangeComparison(list, (s, a) -> comparator.compare(s, a) > 0);
+    }
+
+    public <T> T searchMax(List<T> list, Comparator<T> comparator) {
+        return strangeComparison(list, (s, a) -> comparator.compare(s, a) < 0);
+    }
+
+    private <T> T strangeComparison(List<T> list, BiPredicate<T, T> test) {
         Objects.checkIndex(0, list.size());
         T x = list.get(0);
         for (int i = 1; i < list.size(); i++) {
-            if (function.test(list.get(i), x)) {
+            if (test.test(x, list.get(i))) {
                 x = list.get(i);
             }
         }
         return x;
     }
 
-
-    public static void main(String[] args) {
-        List<Integer> list = List.of(1, 4, 3, 2);
-        MaxMin<Integer> mm = new MaxMin<>();
-        System.out.println(mm.searchMinMax(list, (s, d) -> s < d));
-    }
-
-    @Override
-    public int compareTo(MaxMin o) {
-        return Integer.compare(value, o.value);
-    }
 }
--- src/test/java/ru/job4j/kiss/MaxMinTest.java
+++ src/test/java/ru/job4j/kiss/MaxMinTest.java
@@ -2,6 +2,7 @@ package ru.job4j.kiss;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Comparator;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,8 +13,9 @@ class MaxMinTest {
     void searchMax() {
         List<Integer> list = List.of(1, 4, 3, 2);
         int max = 4;
-        MaxMin<Integer> maxMin = new MaxMin<>();
-        int result = maxMin.searchMinMax(list, (s, d) -> s > d);
+        MaxMin maxMin = new MaxMin();
+        Comparator<Integer> comp = Integer::compareTo;
+        int result = maxMin.searchMax(list, comp);
         assertEquals(max, result);
     }
 
@@ -21,8 +23,10 @@ class MaxMinTest {
     void searchMin() {
         List<Integer> list = List.of(1, 4, 3, 2);
         int min = 1;
-        MaxMin<Integer> maxMin = new MaxMin<>();
-        int result = maxMin.searchMinMax(list, (s, d) -> s < d);
+        MaxMin maxMin = new MaxMin();
+        Comparator<Integer> comp = Integer::compareTo;
+        int result = maxMin.searchMin(list, comp);
         assertEquals(min, result);
     }
+
 }
